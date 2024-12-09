039d99ca2e3d9269cc1c28e1a3c0b85c3779b1fa
Fahadhassan <fahadhassan921@gmail.com>
2024-12-09 13:06:56 +0400

    First Commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3006;
+
+app.get("/", (req, res) => {
+  res.send("Khud say change karo");
+});
+
+app.get("/checking", (req, res) => {
+  res.send("This is Checking Route");
+});
+
+app.listen(PORT, () => console.log(`Sever is running in ${PORT}`));
