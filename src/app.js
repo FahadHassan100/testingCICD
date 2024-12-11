954e9b720e936926c7a662721b22579e09b045c3
Fahadhassan <fahadhassan921@gmail.com>
2024-12-11 15:13:01 +0400

    Edit in health route

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ message: "Everything going good!" });
+  res.status(200).json({ message: "Everything going good!1132" });
 });
 
 app.get("/second", (req, res) => res.send("this is second Route"));
