2e9626d453dd908eb771f69707fb1af3f514411f
Fahadhassan <fahadhassan921@gmail.com>
2024-12-12 14:27:48 +0400

    v6 InshAllah

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ message: "Everything going good! v5" });
+  res.status(200).json({ message: "Everything going good! v6" });
 });
 
 app.get("/second", (req, res) => res.send("this is second Route"));
